cfa67a4f09c45ce9510232eab590fb04c474cb73
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2022-11-02 20:08:53 +0700

    change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/home-work-4'),
+      dir: require('path').join(__dirname, './coverage/home-work'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'home-work-4'`, () => {
+  it(`should have as title 'home-work'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('home-work-4');
+    expect(app.title).toEqual('home-work');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('home-work-4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('home-work app is running!');
   });
 });
